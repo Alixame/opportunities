63042459391e6b49f6d7b11f3fb1d878faf4f671
Lucas Alixame <lucasali2003@gmail.com>
2022-12-31 20:29:07 -0300

    [COR] Model Opportunity

--- Models/Opportunity.php
+++ Models/Opportunity.php
@@ -2,10 +2,23 @@
 
 namespace Alixame\Opportunities\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Opportunity extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $guarded = [];
+
+    // CAMPOS DA TABELA RECUPERADOS EM MASSA
+    protected $fillable = [
+        'name',
+        'function',
+        'unit_price',
+        'unit_of_measure_id'
+    ];
+    
     protected $table = 'opportunities';
 }
